dd2598b9de8166d1585ad6e0cd142af783d98517
tqhdesilva <tdesilvaqihao@gmail.com>
2020-03-17 17:32:38 -0700

    added plots for spectrograms, updated load_data.py

--- load.py
+++ load.py
@@ -7,17 +7,29 @@ THIS_FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(THIS_FILE_DIR, "data/vsb-power-line-fault-detection")
 
 
-def load_train() -> (pd.DataFrame, pd.DataFrame):
-    train_data = pq.read_pandas(os.path.join(DATA_DIR, "train.parquet")).to_pandas()
+def load_train(n_columns: int = None) -> (pd.DataFrame, pd.DataFrame):
+    if n_columns:
+        columns = [str(i) for i in range(n_columns)]
+    train_data = pq.read_pandas(
+        os.path.join(DATA_DIR, "train.parquet"), columns=columns
+    ).to_pandas()
     train_meta = pd.read_csv(
-        os.path.join(DATA_DIR, "metadata_train.csv"), index_col="signal_id"
+        os.path.join(DATA_DIR, "metadata_train.csv"),
+        index_col="signal_id",
+        nrows=n_columns,
     )
     return train_data, train_meta
 
 
-def load_test() -> (pd.DataFrame, pd.DataFrame):
-    test_data = pq.read_pandas(os.path.join(DATA_DIR, "test.parquet")).to_pandas()
+def load_test(n_columns: int = None) -> (pd.DataFrame, pd.DataFrame):
+    if n_columns:
+        columns = [str(i) for i in range(n_columns)]
+    test_data = pq.read_pandas(
+        os.path.join(DATA_DIR, "test.parquet"), columns=columns
+    ).to_pandas()
     test_meta = pd.read_csv(
-        os.path.join(DATA_DIR, "metadata_test.csv"), index_col="signal_id"
+        os.path.join(DATA_DIR, "metadata_test.csv"),
+        index_col="signal_id",
+        nrows=n_columns,
     )
     return test_data, test_meta
