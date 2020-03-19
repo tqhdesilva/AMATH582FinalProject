26d7102d588327ee5b18f44b38a275b218e28a04
tqhdesilva <tdesilvaqihao@gmail.com>
2020-03-18 23:08:32 -0700

    removed some unused code

--- pipeline.py
+++
@@ -1,9 +0,0 @@
-from sklearn.pipeline import Pipeline, make_pipeline
-from sklearn.preprocessing import FunctionTransformer, StandardScaler 
-from sklearn.decomposition import PCA
-
-
-pre_pipeline = make_pipeline(
-    StandardScaler(),
-    PCA(n_components=117),
-)
